ed33865ed262999c7712a458e18374fb829a06b2
RobellS <94008085+RobellS@users.noreply.github.com>
2021-12-22 16:02:04 -0500

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Creating ship class to reference later on in code.
 class Ship {
   constructor(hull, firepower, accuracy) {
     this.hull = hull;
@@ -14,14 +15,16 @@ class Ship {
     };
   }
 }
+//Created a child class for the enemy ship.
 class collectorShip extends Ship {
   constructor(hull, firepower, accuracy) {
     super(hull, firepower, accuracy);
   }
 }
-let collectorArr = [];
-let ussNormandy = new Ship(20, 5, 0.7);
 
+
+let ussNormandy = new Ship(20, 5, 0.7); // Player's hull = 20; Player's firepower = 5; Player's accuracy = 70%
+let collectorArr = []; // Declar & Init an empty array to .push enemy ship objects into while creating in the loop.
 for (let numofShip = 0; numofShip < 6; numofShip++) {
   let collector = new collectorShip(
     Math.floor(Math.random() * 4) + 3,
@@ -30,15 +33,15 @@ for (let numofShip = 0; numofShip < 6; numofShip++) {
   );
   collectorArr.push(collector);
 }
-// enemy's hull= Math.floor(Math.random() * 4) + 3,
-// enemy's firepower = Math.floor(Math.random() * 3) + 2,
-// enemy's accuracy = Math.random() * (0.8 - 0.6) + 0.6
+// Enemy's hull = Math.floor(Math.random() * 4) + 3,
+// Enemy's firepower = Math.floor(Math.random() * 3) + 2,
+// Enemy's accuracy = Math.random() * (0.8 - 0.6) + 0.6
 
 let i = 0;
 const attackRetreat = prompt(
   `Attacking Ship #${i + 1} or retreating? [Respond attack/retreat]`
 );
-
+// creating a function that simulates battle with prompts for player to make decisions (i.e Attack/Retreat)
 function game() {
   while (i < 6) {
     if (attackRetreat.toLowerCase() == "attack") {
@@ -56,16 +59,18 @@ function game() {
       } else {
         alert("On to the next round...");
       }
-    } else if (attackRetreat.toLowerCase() == "retreat") {
+    }
+    //End of the attack and the start of the retreat conditional. 
+    else if (attackRetreat.toLowerCase() == "retreat") {
       alert("You have escaped.");
       return;
     } else {
       alert("Wrong input. Reload and try again.");
       return;
     }
-  }
-}
+  } //while loop ends here
+} // function ends here
 
-game();
-console.log(ussNormandy.hull);
-console.log(collectorArr);
+game();// calling function that simulates battle to run the game.
+console.log(ussNormandy.hull);// Checking the Player health at the end of the simulation.
+console.log(collectorArr);// checking how many objects are left in the array (Should be zero).
